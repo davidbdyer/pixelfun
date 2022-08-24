563402a8c612feae72b8cce5ac3ef9d8de6389d5
davidbdyer <social_davidbdyer@icloud.com>
2022-08-24 16:48:07 +0000

    added color pickers to html and javascript.

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,83 @@ const canvas = document.querySelector('#container__canvas');
 const brushBtn = document.querySelector('#btn__brush');
 const eraserBtn = document.querySelector('#btn__eraser');
 const clearBtn = document.querySelector('#btn__clear');
+const colorPickers = document.querySelectorAll('.color-picker');
 
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
 const squares = 500;
 
 let currentTool = 'brush';
 let isPressed = false;
+
+// --------------------
+// - functions declared
+// --------------------
+function setColor(element, active) {
+	if (active === true) {
+		const color = getRandomColor();
+		element.style.background = color;
+		element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+	}
+}
+
+function removeColor(element, active) {
+	if (active === true) {
+		element.style.background = '#1d1d1d';
+		element.style.boxShadow = '0 0 2px #000';
+	}
+}
+
+function getRandomColor() {
+	return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function setTool(tool) {
+	currentTool = tool;
+}
+
+function useTool(elm, active) {
+	if (currentTool == 'brush') {
+		setColor(elm, active);
+	} else if (currentTool == 'eraser') {
+		removeColor(elm, active);
+	}
+}
+
+function clearCanvas() {
+	const squares = document.querySelectorAll('.square');
+	for (let square of squares) {
+		removeColor(square, true);
+	}
+}
+
+// target element
+function findTarget(e, inputType) {
+	const cords = { x: 0, y: 0 }
+
+	if (inputType === 'touch') {
+		cords.x = e.touches[0].clientX; // Horizontal
+		cords.y = e.touches[0].clientY; // Vertical
+	} else if (inputType === 'mouse') {
+		cords.x = e.clientX; // Horizontal
+		cords.y = e.clientY; // Vertical
+	}
+	return document.elementFromPoint(cords.x, cords.y);
+}
+
+// color picker related
+function setColorPickers() {
+	const pickerArray = Array.from(colorPickers);
+
+	for (let picker of pickerArray) {
+		const i = pickerArray.indexOf(picker);
+		picker.value = colors[i]
+	}
+}
+
+
+// ------------------------------------
+// functions called and event listeners
+// ------------------------------------
 
 // Generate squares for canvas.
 for (let i = 0; i < squares; i++) {
@@ -16,6 +87,9 @@ for (let i = 0; i < squares; i++) {
 
 	canvas.appendChild(square);
 }
+
+// set initial color picker colors to default color array
+setColorPickers()
 
 // Add event listener to canvas
 // mouse listeners
@@ -76,59 +150,3 @@ eraserBtn.addEventListener('click', () => {
 clearBtn.addEventListener('click', () => {
 	clearCanvas();
 });
-
-
-// --------------------
-// - functions declared
-// --------------------
-function setColor(element, active) {
-	if (active === true) {
-		const color = getRandomColor();
-		element.style.background = color;
-		element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-	}
-}
-
-function removeColor(element, active) {
-	if (active === true) {
-		element.style.background = '#1d1d1d';
-		element.style.boxShadow = '0 0 2px #000';
-	}
-}
-
-function getRandomColor() {
-	return colors[Math.floor(Math.random() * colors.length)];
-}
-
-function setTool(tool) {
-	currentTool = tool;
-}
-
-function useTool(elm, active) {
-	if (currentTool == 'brush') {
-		setColor(elm, active);
-	} else if (currentTool == 'eraser') {
-		removeColor(elm, active);
-	}
-}
-
-function clearCanvas() {
-	const squares = document.querySelectorAll('.square');
-	for (let square of squares) {
-		removeColor(square, true);
-	}
-}
-
-// target element
-function findTarget(e, inputType) {
-	const cords = { x: 0, y: 0 }
-
-	if (inputType === 'touch') {
-		cords.x = e.touches[0].clientX; // Horizontal
-		cords.y = e.touches[0].clientY; // Vertical
-	} else if (inputType === 'mouse') {
-		cords.x = e.clientX; // Horizontal
-		cords.y = e.clientY; // Vertical
-	}
-	return document.elementFromPoint(cords.x, cords.y);
-}

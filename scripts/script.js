93293525053731b3d74da0360bfba9e4955257e1
davidbdyer <social_davidbdyer@icloud.com>
2022-08-24 20:28:28 +0000

    singe and multi color modes

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,38 @@ const canvas = document.querySelector('#container__canvas');
 const brushBtn = document.querySelector('#btn__brush');
 const eraserBtn = document.querySelector('#btn__eraser');
 const clearBtn = document.querySelector('#btn__clear');
+const oneColorBtn = document.querySelector('#btn__one-color');
+const toolBtns = document.querySelectorAll('.btn-tool');
 const colorPickers = document.querySelectorAll('.color-picker');
 
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
 const squares = 500;
 
-let currentTool = 'brush';
+let currentTool = brushBtn;
 let isPressed = false;
+let singleColorMode = false;
 
 // --------------------
 // - functions declared
 // --------------------
+
+// color functions
+function getRandomColor() {
+	return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function getToolColor(mode) {
+	if (mode === false) {
+		return getRandomColor()
+	} else {
+		return colors[0];
+	}
+}
+
 function setColor(element, active) {
 	if (active === true) {
-		const color = getRandomColor();
+		// const color = getRandomColor();
+		const color = getToolColor(singleColorMode);
 		element.style.background = color;
 		element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 	}
@@ -28,18 +46,46 @@ function removeColor(element, active) {
 	}
 }
 
-function getRandomColor() {
-	return colors[Math.floor(Math.random() * colors.length)];
+// color picker related
+function setColorPickersColor() {
+	const pickerArray = Array.from(colorPickers);
+
+	for (let picker of pickerArray) {
+		const i = pickerArray.indexOf(picker);
+		picker.value = colors[i];
+	}
+}
+
+colorPickers.forEach((elm) => {
+	elm.addEventListener('change', (e) => {
+		const index = e.target.id.slice(-1) - 1;
+		const newClr = e.target.value;
+		colors[index] = newClr;
+	});
+});
+
+// tools functions
+function showActiveTool(elm, array) {
+	if (array) {
+		array.forEach((item) => {
+			item.classList.remove('active');
+		});
+	} else if (elm.classList.contains('active')) {
+		elm.classList.remove('active');
+		return;
+	}
+	elm.classList.add('active');
 }
 
 function setTool(tool) {
 	currentTool = tool;
+	showActiveTool(tool, toolBtns);
 }
 
 function useTool(elm, active) {
-	if (currentTool == 'brush') {
+	if (currentTool == brushBtn) {
 		setColor(elm, active);
-	} else if (currentTool == 'eraser') {
+	} else if (currentTool == eraserBtn) {
 		removeColor(elm, active);
 	}
 }
@@ -51,9 +97,13 @@ function clearCanvas() {
 	}
 }
 
+function changeElmText(elm, string) {
+	elm.textContent = string;
+}
+
 // target element
 function findTarget(e, inputType) {
-	const cords = { x: 0, y: 0 }
+	const cords = { x: 0, y: 0 };
 
 	if (inputType === 'touch') {
 		cords.x = e.touches[0].clientX; // Horizontal
@@ -65,30 +115,27 @@ function findTarget(e, inputType) {
 	return document.elementFromPoint(cords.x, cords.y);
 }
 
-// color picker related
-function setColorPickersColor() {
-	const pickerArray = Array.from(colorPickers);
-
-	for (let picker of pickerArray) {
-		const i = pickerArray.indexOf(picker);
-		picker.value = colors[i]
+function tggleElmArryVisAfterIdx(hide, startIdx = 0) {
+	if (hide === true) {
+		colorPickers.forEach((elm, idx) => {
+			if (idx !== startIdx - 1) {
+				elm.style.display = 'none';
+			}
+		});
+	} else {
+		colorPickers.forEach((elm, idx) => {
+			if (idx !== startIdx - 1) {
+				elm.style.removeProperty('display');
+			}
+		});
 	}
 }
 
-colorPickers.forEach((elm) => {
-	elm.addEventListener('change', (e) => {
-		const index = (e.target.id.slice(-1) - 1);
-		const newClr = e.target.value;
-		colors[index] = newClr;
-	})
-})
-
-
-
 // ------------------------------------
-// functions called and event listeners
+// Functions called and event listeners.
 // ------------------------------------
 
+// Dom creation
 // Generate squares for canvas.
 for (let i = 0; i < squares; i++) {
 	const square = document.createElement('div');
@@ -97,11 +144,11 @@ for (let i = 0; i < squares; i++) {
 	canvas.appendChild(square);
 }
 
-// set initial color picker colors to default color array
-setColorPickersColor()
+// Set initial color pickers color to default color array.
+setColorPickersColor();
 
-// Add event listener to canvas
-// mouse listeners
+// Add event listener to canvas.
+// Mouse listeners.
 canvas.addEventListener('mousedown', (e) => {
 	e.preventDefault();
 	isPressed = true;
@@ -151,13 +198,23 @@ canvas.addEventListener('touchend', () => {
 
 // Controls
 brushBtn.addEventListener('click', () => {
-	setTool('brush');
+	setTool(brushBtn);
 });
 eraserBtn.addEventListener('click', () => {
-	setTool('eraser');
+	setTool(eraserBtn);
 });
 clearBtn.addEventListener('click', () => {
 	clearCanvas();
 });
 
-// color pickers
+oneColorBtn.addEventListener('click', () => {
+	if (singleColorMode === false) {
+		singleColorMode = true;
+		changeElmText(oneColorBtn, '5 Colors')
+		tggleElmArryVisAfterIdx(true, 1);
+		return;
+	}
+	singleColorMode = false;
+	changeElmText(oneColorBtn, '1 Color')
+	tggleElmArryVisAfterIdx(false);
+});
